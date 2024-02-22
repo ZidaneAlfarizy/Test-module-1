7f38f5939735408dcf31d67a87c5b6338f7934a5
ZidaneAlfarizy <alfarizy143@gmail.com>
2024-02-22 15:25:10 +0700

    first commit

---
+++ modul0/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+public class modulPractice {
+    public static void main(String[] args) {
+        System.out.print("ini teks yang dicetak dengan print()");
+        System.out.println("sedangkan ini teks yang dicetak dengan
+                println()");
+                System.out.print("pake print() lagi");
+    }
+}
+rsrai
